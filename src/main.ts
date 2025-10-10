e154011b512fd03b14c686d5fb5aa865c5372aa6
Jay <kred110396@gmail.com>
2025-10-10 18:53:08 +0800

    chore: remove vue query

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,11 @@ import './assets/styles/all.scss'
 
 import 'bootstrap/js/dist/collapse'
 
-import { VueQueryPlugin } from '@tanstack/vue-query'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
 app.use(router)
-app.use(VueQueryPlugin)
 
 app.mount('#app')
